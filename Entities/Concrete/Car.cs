f65f724c0e24b8654cf27f00b84b57fc92f42bf5
gulceselim <selimgulce@hotmail.com>
2021-02-01 16:29:14 +0300

    Created files for Entities Layer

--- Entities/Concrete/Car.cs
+++ Entities/Concrete/Car.cs
@@ -10,7 +10,7 @@ namespace Entities.Concrete
         public int Id { get; set; }
         public int BrandId { get; set; }
         public int ColorId { get; set; }
-        public int ModelYear { get; set; }
+        public string ModelYear { get; set; }
         public decimal DailyPrice { get; set; }
         public string Decription { get; set; }
     }
