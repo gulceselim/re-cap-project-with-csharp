152701994461caf3f33df44ee23edf7776f16b18
gulceselim <selimgulce@hotmail.com>
2021-02-04 18:50:33 +0300

    SQL query file added✔

---
+++ RentACarSQLQuery.sql
@@ -0,0 +1,41 @@
+﻿CREATE TABLE Cars(
+	CarID int PRIMARY KEY IDENTITY(1,1),
+	BrandID int,
+	ColorID int,
+	ModelYear nvarchar(25),
+	DailyPrice decimal,
+	Descriptions nvarchar(25),
+	FOREIGN KEY (BrandID) REFERENCES Colors(ColorID),
+	FOREIGN KEY (ColorID) REFERENCES Brands(BrandID)
+)
+
+CREATE TABLE Colors(
+	ColorID int PRIMARY KEY IDENTITY(1,1),
+	ColorName nvarchar(25),
+)
+
+CREATE TABLE Brands(
+	BrandID int PRIMARY KEY IDENTITY(1,1),
+	BrandName nvarchar(25),
+)
+
+
+INSERT INTO Cars(BrandID,ColorID,ModelYear,DailyPrice,Descriptions)
+VALUES
+	('1','2','2012','100','Manuel Benzin'),
+	('1','3','2015','150','Otomatik Dizel'),
+	('2','1','2017','200','Otomatik Hybrid'),
+	('3','3','2014','125','Manuel Dizel');
+
+INSERT INTO Colors(ColorName)
+VALUES
+	('Beyaz'),
+	('Siyah'),
+	('Mavi');
+
+
+INSERT INTO Brands(BrandName)
+VALUES
+	('Mercedes'),
+	('BMW'),
+	('Renault');
