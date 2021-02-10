514b603ced1f9cde017663027c2f2fd436177e72
gulceselim <selimgulce@hotmail.com>
2021-02-10 12:29:11 +0300

    Concrete classes refactored in Entities Layer✔

--- Entities/DTOs/CarDetailDto.cs
+++ Entities/DTOs/CarDetailDto.cs
@@ -1,17 +1,17 @@
-﻿using Core.Entities;
+﻿using Core;
 using System;
 using System.Collections.Generic;
 using System.Text;
 
-namespace Entities.Concrete
+namespace Entities.DTOs
 {
-    public class Car : IEntity
+    public class CarDetailDto : IDto
     {
         public int CarId { get; set; }
-        public string ModelYear { get; set; }
+        public string BrandName { get; set; }
+        public string ColorName { get; set; }
         public decimal DailyPrice { get; set; }
+        public string ModelYear { get; set; }
         public string Descriptions { get; set; }
-        public int BrandId { get; set; }
-        public int ColorId { get; set; }
     }
 }
