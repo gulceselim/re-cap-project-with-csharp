d30693e22b635a799bd3250ad2f9a299211d4b52
gulceselim <selimgulce@hotmail.com>
2021-02-10 12:32:19 +0300

    Created Core Layer✔

---
+++ Core/DataAccess/IEntityRepository.cs
@@ -0,0 +1,18 @@
+﻿using Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace Core.DataAccess
+{
+    public interface IEntityRepository<T> where T : class, IEntity, new()
+    {
+        void Add(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+        List<T> GetAll(Expression<Func<T,bool>> filter = null);
+        T Get(Expression<Func<T, bool>> filter);
+
+    }
+}
