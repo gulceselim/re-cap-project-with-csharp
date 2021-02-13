a6936cb8cb78da662f78af3caea5f61e5a507c85
gulceselim <selimgulce@hotmail.com>
2021-02-13 14:57:18 +0300

    EfCarDal file refactored✔

--- DataAccess/Concrete/EntityFramework/Repository/EfCarDal.cs
+++ DataAccess/Concrete/EntityFramework/Repository/EfCarDal.cs
@@ -14,11 +14,11 @@ namespace DataAccess.Concrete.EntityFramework
 {
     public class EfCarDal : EfEntityRepositoryBase<Car, RentACarContext>, ICarDal
     {
-        public List<CarDetailDto> GetCarDetails()
+        public List<CarDetailDto> GetCarDetails(Expression<Func<Car, bool>> filter = null)
         {
             using (RentACarContext context = new RentACarContext())
             {
-                var result = from c in context.Cars
+                var result = from c in filter == null ? context.Cars : context.Cars.Where(filter)
                              join co in context.Colors
                              on c.ColorId equals co.ColorId
                              join b in context.Brands
