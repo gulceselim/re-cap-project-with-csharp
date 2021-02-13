1c66ed373d7388193bbe7db2ffd1dfdafadd53cd
gulceselim <selimgulce@hotmail.com>
2021-02-13 14:56:33 +0300

    DbSet attributes created for Customer, Rental and User✔

--- DataAccess/Concrete/EntityFramework/Context/RentACarContext.cs
+++ DataAccess/Concrete/EntityFramework/Context/RentACarContext.cs
@@ -17,7 +17,9 @@ namespace DataAccess.Concrete.EntityFramework.Context
         public DbSet<Car> Cars { get; set; }
         public DbSet<Brand> Brands { get; set; }
         public DbSet<Color> Colors { get; set; }
-
+        public DbSet<User> Users { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Rental> Rentals { get; set; }
     }
 
 }
