0fc967c2e24a6150592a903706ab838b3f994cb3
gulceselim <selimgulce@hotmail.com>
2021-02-13 14:58:01 +0300

    Entity Framework files created for Customer, Rental, User✔

---
+++ DataAccess/Concrete/EntityFramework/Repository/EfCustomerDal.cs
@@ -0,0 +1,14 @@
+﻿using Core.DataAccess.EntityFramework;
+using DataAccess.Abstract;
+using DataAccess.Concrete.EntityFramework.Context;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess.Concrete.EntityFramework.Repository
+{
+    public class EfCustomerDal : EfEntityRepositoryBase<Customer,RentACarContext>, ICustomerDal
+    {
+    }
+}
---
+++ DataAccess/Concrete/EntityFramework/Repository/EfRentalDal.cs
@@ -0,0 +1,42 @@
+﻿using Core.DataAccess.EntityFramework;
+using DataAccess.Abstract;
+using DataAccess.Concrete.EntityFramework.Context;
+using Entities.Concrete;
+using Entities.DTOs;
+using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq.Expressions;
+
+namespace DataAccess.Concrete.EntityFramework.Repository
+{
+    public class EfRentalDal : EfEntityRepositoryBase<Rental, RentACarContext>, IRentalDal
+    {
+        public List<RentalDetailDto> GetCarDetails(Expression<Func<Rental,bool>> filter = null)
+        {
+            using (RentACarContext context = new RentACarContext())
+            {
+                var result = from r in filter == null ? context.Rentals : context.Rentals.Where(filter)
+                             join c in context.Cars
+                             on r.CarId equals c.CarId
+                             join cu in context.Customers
+                             on r.CustomerId equals cu.CustomerId
+                             join b in context.Brands
+                             on c.BrandId equals b.BrandId
+                             join u in context.Users
+                             on cu.UserId equals u.UserId
+                             select new RentalDetailDto
+                             {
+                                 RentalId = r.RentalId,
+                                 CarName = b.BrandName,
+                                 CustomerName = cu.CustomerName,
+                                 UserName = u.FirstName + " " + u.LastName,
+                                 RentDate = r.RentDate,
+                                 ReturnDate = r.ReturnDate
+                             };
+                return result.ToList();
+            }
+        }
+    }
+}
