874b2b06dea9e6c359480f957c764015bd3bca05
gulceselim <selimgulce@hotmail.com>
2021-02-13 14:55:38 +0300

    Created utilities file that is Results✔

---
+++ Core/Utilities/Results/ErrorResult.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Utilities.Results
+{
+    public class ErrorResult : Result
+    {
+        public ErrorResult(string message) : base(false, message)
+        {
+        }
+        public ErrorResult() : base(false)
+        {
+        }
+    }
+}
---
+++ Core/Utilities/Results/SuccessDataResult.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Utilities.Results
+{
+    public class SuccessDataResult<T> : DataResult<T>
+    {
+        public SuccessDataResult(T data) : base(data, true)
+        {
+        }
+
+        public SuccessDataResult(T data, string message) : base(data, true, message)
+        {
+        }
+
+        public SuccessDataResult(string message) : base(default, true, message)
+        {
+
+        }
+        public SuccessDataResult() : base(default,true)
+        {
+
+        }
+    }
+}
