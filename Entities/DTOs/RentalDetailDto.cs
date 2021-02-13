0c0e9a9858161bfb8dc4be8d3ae6987e4717e3f2
gulceselim <selimgulce@hotmail.com>
2021-02-13 14:51:16 +0300

    Add Dto class for Rental✔

---
+++ Entities/DTOs/RentalDetailDto.cs
@@ -0,0 +1,17 @@
+﻿using Core;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.DTOs
+{
+    public class RentalDetailDto : IDto
+    {
+        public int RentalId { get; set; }
+        public string CarName { get; set; }
+        public string UserName { get; set; }
+        public string CustomerName { get; set; }
+        public DateTime RentDate { get; set; }
+        public DateTime? ReturnDate { get; set; }
+    }
+}
