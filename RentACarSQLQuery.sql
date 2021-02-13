d70d88eee06a203c5e77b31e6d35b18491fcf178
gulceselim <selimgulce@hotmail.com>
2021-02-13 14:58:31 +0300

    SQL queries refactored✔

--- RentACarSQLQuery.sql
+++ RentACarSQLQuery.sql
@@ -1,26 +1,55 @@
 ﻿CREATE TABLE Cars(
-	CarID int PRIMARY KEY IDENTITY(1,1),
-	BrandID int,
-	ColorID int,
+	CarId int PRIMARY KEY IDENTITY(1,1),
+	BrandId int,
+	ColorId int,
 	ModelYear nvarchar(25),
 	DailyPrice decimal,
 	Descriptions nvarchar(25),
-	FOREIGN KEY (ColorID) REFERENCES Colors(ColorID),
-	FOREIGN KEY (BrandID) REFERENCES Brands(BrandID)
+	FOREIGN KEY (ColorId) REFERENCES Colors(ColorId),
+	FOREIGN KEY (BrandId) REFERENCES Brands(BrandId)
 )
 
 CREATE TABLE Colors(
-	ColorID int PRIMARY KEY IDENTITY(1,1),
+	ColorId int PRIMARY KEY IDENTITY(1,1),
 	ColorName nvarchar(25),
 )
 
 CREATE TABLE Brands(
-	BrandID int PRIMARY KEY IDENTITY(1,1),
+	BrandId int PRIMARY KEY IDENTITY(1,1),
 	BrandName nvarchar(25),
 )
 
+CREATE TABLE Users(
+	UserId int PRIMARY KEY IDENTITY(1,1),
+	FirstName nvarchar(25),
+	LastName nvarchar(25),
+	Email nvarchar(55),
+	Password nvarchar(35),
+)
 
-INSERT INTO Cars(BrandID,ColorID,ModelYear,DailyPrice,Descriptions)
+
+CREATE TABLE Customers(
+	CustomerId int PRIMARY KEY IDENTITY(1,1),
+	UserId int,
+	CustomerName nvarchar(25),
+	FOREIGN KEY (UserId) REFERENCES Users(UserId),
+)
+
+CREATE TABLE Rentals(
+	RentalId int PRIMARY KEY IDENTITY(1,1),
+	CarId int,
+	CustomerID int,
+	RentDate datetime,
+	ReturnDate datetime,
+	FOREIGN KEY (CarId) REFERENCES Cars(CarId),
+	FOREIGN KEY (CustomerId) REFERENCES Customers(CustomerId),
+
+)
+
+
+
+
+INSERT INTO Cars(BrandId,ColorId,ModelYear,DailyPrice,Descriptions)
 VALUES
 	('1','2','2012','100','Manuel Benzin'),
 	('1','3','2015','150','Otomatik Dizel'),
@@ -39,3 +68,4 @@ VALUES
 	('Tesla'),
 	('BMW'),
 	('Renault');
+
