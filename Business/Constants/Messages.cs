9bddfdc6b48639704a170ae493cc05dee3b48ce2
gulceselim <selimgulce@hotmail.com>
2021-02-13 14:53:57 +0300

    Created constant file that is Messages✔

---
+++ Business/Constants/Messages.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Constants
+{
+    public static class Messages
+    {
+        public static string AddedBrand = "Marka başarıyla eklendi.";
+        public static string DeletedBrand = "Marka başarıyla silindi.";
+        public static string UpdatedBrand = "Marka başarıyla güncellendi.";
+        public static string FailedBrandAddOrUpdate = "Lütfen marka isminin uzunluğunu 2 karakterden fazla giriniz.";
+
+        public static string AddedCar = "Araba başarıyla eklendi.";
+        public static string DeletedCar = "Araba başarıyla silindi.";
+        public static string UpdatedCar = "Araba başarıyla güncellendi.";
+        public static string FailedCarAddOrUpdate = "Lütfen günlük fiyat kısmını 0'dan büyük giriniz.";
+
+        public static string AddedColor = "Renk başarıyla eklendi.";
+        public static string DeletedColor = "Renk başarıyla silindi.";
+        public static string UpdatedColor = "Renk başarıyla güncellendi.";
+
+        public static string AddedCustomer = "Müşteri başarıyla eklendi.";
+        public static string DeletedCustomer = "Müşteri başarıyla silindi.";
+        public static string UpdatedCustomer = "Müşteri başarıyla güncellendi.";
+
+
+        public static string AddedUser = "Kullanıcı başarıyla eklendi.";
+        public static string DeletedUser = "Kullanıcı başarıyla silindi.";
+        public static string UpdatedUser = "Kullanıcı başarıyla güncellendi.";
+
+
+        public static string AddedRental = "Araba Kiralama işlemi baraşıyla gerçekleşti.";
+        public static string DeletedRental = "Araba Kiralama işlemi iptal edildi.";
+        public static string UpdatedRental = "Araba Kiralama işlemi güncellendi.";
+        public static string FailedRentalAddOrUpdate = "Bu araba henüz teslim edilmediği için kiralayamazsınız.";
+        public static string ReturnedRental = "Kiraladığınız araç teslim edildi.";
+
+
+
+
+
+    }
+}
