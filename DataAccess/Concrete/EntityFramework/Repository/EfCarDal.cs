270199717c550214b6672649bd375a8d6de0a9c1
gulceselim <selimgulce@hotmail.com>
2021-02-15 17:37:19 +0300

    Fixed namespace of entities layer✔

--- DataAccess/Concrete/EntityFramework/Repository/EfCarDal.cs
+++ DataAccess/Concrete/EntityFramework/Repository/EfCarDal.cs
@@ -10,7 +10,7 @@ using System.Linq;
 using System.Linq.Expressions;
 using System.Text;
 
-namespace DataAccess.Concrete.EntityFramework
+namespace DataAccess.Concrete.EntityFramework.Repository
 {
     public class EfCarDal : EfEntityRepositoryBase<Car, RentACarContext>, ICarDal
     {
