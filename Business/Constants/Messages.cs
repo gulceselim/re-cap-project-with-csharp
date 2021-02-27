88cada1b79b427cb480f222e60e50e8d6e155696
gulceselim <selimgulce@hotmail.com>
2021-02-27 16:27:23 +0300

    Created messages for CarImages✔

--- Business/Constants/Messages.cs
+++ Business/Constants/Messages.cs
@@ -36,7 +36,10 @@ namespace Business.Constants
         public static string FailedRentalAddOrUpdate = "Bu araba henüz teslim edilmediği için kiralayamazsınız.";
         public static string ReturnedRental = "Kiraladığınız araç teslim edildi.";
 
-
+        public static string AddedCarImage = "Araba için yüklenilen resim başarıyla eklendi.";
+        public static string DeletedCarImage = "Arabanın resmi başarıyla silindi.";
+        public static string UpdatedCarImage = "Araba için yüklenilen resim başarıyla güncellendi.";
+        public static string FailedCarImageAdd = "Bir araba 5'den fazla resme sahip olamaz.";
 
 
 
