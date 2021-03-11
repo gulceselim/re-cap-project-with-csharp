742c0a0bebcb4c3c77b749590fd9a4a3a5848c38
gulceselim <selimgulce@hotmail.com>
2021-03-11 17:28:41 +0300

    Add some options for Cors for working on API✔

--- WebAPI/Startup.cs
+++ WebAPI/Startup.cs
@@ -39,6 +39,8 @@ namespace WebAPI
         {
             services.AddControllers();
 
+            services.AddCors();
+
             var tokenOptions = Configuration.GetSection("TokenOptions").Get<TokenOptions>();
 
             services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
@@ -68,6 +70,8 @@ namespace WebAPI
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.UseCors(builder=> builder.WithOrigins("http://localhost:4200").AllowAnyHeader());
 
             app.UseHttpsRedirection();
 
